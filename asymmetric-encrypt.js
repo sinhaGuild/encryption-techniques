f5c5054102ff1eded13bc52f38c32c73e413dd0d
sinhaGuild <anurag.r.sinha@gmail.com>
2022-08-16 11:21:43 -0400

    cleanup and ignore files

--- asymmetric-encrypt.js
+++ asymmetric-encrypt.js
@@ -4,12 +4,14 @@ require("colors");
 
 const message = "Jai Ganesh Deva";
 
+//encrypt data using public key
 const encryptedData = publicEncrypt(publicKey, Buffer.from(message));
 
 console.log(
   `Encrypted Message: ${encryptedData.toString("base64")}`.bgCyan.italic
 );
 
+//data can only be decrypted using private key
 const decryptedData = privateDecrypt(privateKey, encryptedData);
 
 console.log(
